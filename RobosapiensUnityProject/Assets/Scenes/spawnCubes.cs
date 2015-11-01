03a577de0bad482321d75075a659631751bb5938
Ooya <thibaut.castanie@gmail.com>
2015-11-01 23:45:35 +0100

    Ajout de cubes par un clic
    Peut être amélioré

---
+++ RobosapiensUnityProject/Assets/Scenes/spawnCubes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnCubes : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			// print(Input.mousePosition);
+			// print(GetComponent<Transform>().position);
+			// Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,1.0f));
+			Instantiate(cube,GetComponent<Transform>().position,Quaternion.identity);
+		}
+	}
+}
