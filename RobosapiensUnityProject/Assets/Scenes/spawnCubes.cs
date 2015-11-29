b157cfb61df4f8fb87c7f3333234e149a63df143
Ooya <thibaut.castanie@gmail.com>
2015-11-29 20:41:08 +0100

    Merge manuel

--- RobosapiensUnityProject/Assets/Scenes/spawnCubes.cs
+++ RobosapiensUnityProject/Assets/Scenes/spawnCubes.cs
@@ -4,32 +4,24 @@ using Leap;
 
 public class spawnCubes : MonoBehaviour {
 
-<<<<<<< HEAD
-	public GameObject cube;
-	public GameObject cubeFantome;
-    public HandController myLeap;
-=======
     public GameObject cube;
     public GameObject cubeFantome;
+    public HandController myLeap;
     public GameObject mur;
     public GameObject murFantome;
     public GameObject healthPack;
     public GameObject healthPackFantome;
->>>>>>> eff3fbe5c6cd073a5a35505d8ac3f734d71033aa
 
     int idObjet;            //0 = cube, 1 = mur, 2 = healthPack
     Quaternion rotation;
+    private bool hover = false;
 
-<<<<<<< HEAD
-    private bool hover=false;
+    void Start() {
+        idObjet = 0;
+        //myLeap.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+    }
 
-	// Use this for initialization
-	void Start () {
-//        myLeap.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
-	}
-
-    void onHover()
-    {
+    void onHover() {
         Debug.Log("hover");
         hover = true;
     }
@@ -37,27 +29,16 @@ public class spawnCubes : MonoBehaviour {
 
 
     // Update is called once per frame
-    void Update () {
 
-        rotation = Quaternion.Euler(0, GameObject.Find("cubeSpawner").transform.rotation.eulerAngles.y, 0);
 
-        if (Input.GetMouseButtonDown(0)){
-			Instantiate(cubeFantome,transform.position, rotation);
-            hover = false;
-		}
-=======
-    // Use this for initialization
-    void Start() {
-        idObjet = 0;
-    }
 
-    // Update is called once per frame
     void Update() {
 
         if (Input.GetKeyDown(KeyCode.C)) {
-            if(idObjet == 0 || idObjet == 1) {
+            if (idObjet == 0 || idObjet == 1) {
                 idObjet += 1;
-            } else {
+            }
+            else {
                 idObjet = 0;
             }
         }
@@ -68,6 +49,7 @@ public class spawnCubes : MonoBehaviour {
             switch (idObjet) {
                 case 0:
                     Instantiate(cubeFantome, transform.position, rotation);
+                    hover = false;
                     break;
                 case 1:
                     Instantiate(murFantome, transform.position, rotation);
@@ -81,7 +63,6 @@ public class spawnCubes : MonoBehaviour {
             }
             //Instantiate(cubeFantome,transform.position, rotation);
         }
->>>>>>> eff3fbe5c6cd073a5a35505d8ac3f734d71033aa
 
         if (Input.GetMouseButtonUp(0)) {
             switch (idObjet) {
