2231575119f6a2d20b1f192c8ea09f687cbbc302
Francois-Suro <fsuro@hotmail.fr>
2015-12-08 00:16:05 +0100

    bot type 2

--- robosapiensBrainServ_2/src/robosapiensNeuralNetwork/GenomeManager.java
+++ robosapiensBrainServ_2/src/robosapiensNeuralNetwork/GenomeManager.java
@@ -12,8 +12,8 @@ public class GenomeManager implements Runnable{
 	public GenomeManager()
 	{
 		NeuralNetGlobals.genAlg = new GeneticAlgorithm();
-		int totalWeights = NeuralNetGlobals.FEELER_COUNT * NeuralNetGlobals.hiddenLayerSize + NeuralNetGlobals.hiddenLayerSize * NeuralNetGlobals.NN_OUTPUT_COUNT + NeuralNetGlobals.hiddenLayerSize + NeuralNetGlobals.NN_OUTPUT_COUNT+10;
-		System.out.println(NeuralNetGlobals.FEELER_COUNT * NeuralNetGlobals.hiddenLayerSize + NeuralNetGlobals.hiddenLayerSize * NeuralNetGlobals.NN_OUTPUT_COUNT + NeuralNetGlobals.hiddenLayerSize + NeuralNetGlobals.NN_OUTPUT_COUNT);
+		int totalWeights = NeuralNetGlobals.FEELER_COUNT * (NeuralNetGlobals.hiddenLayerSize+1) + (NeuralNetGlobals.hiddenLayerSize+1) * NeuralNetGlobals.NN_OUTPUT_COUNT + NeuralNetGlobals.hiddenLayerSize + NeuralNetGlobals.NN_OUTPUT_COUNT;
+		System.out.println(NeuralNetGlobals.FEELER_COUNT * (NeuralNetGlobals.hiddenLayerSize+1) + (NeuralNetGlobals.hiddenLayerSize+1) * NeuralNetGlobals.NN_OUTPUT_COUNT + NeuralNetGlobals.hiddenLayerSize + NeuralNetGlobals.NN_OUTPUT_COUNT);
 		NeuralNetGlobals.genAlg.GenerateNewPopulation(NeuralNetGlobals.MAX_GENOME_POPULATION, totalWeights);
 	}
 
