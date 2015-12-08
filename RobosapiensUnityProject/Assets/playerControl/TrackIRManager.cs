8b02af89269df8155bab9ad3d2e01060b099e041
Francois-Suro <fsuro@hotmail.fr>
2015-12-08 23:21:20 +0100

    début trackir

---
+++ RobosapiensUnityProject/Assets/playerControl/TrackIRManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackIRManager : MonoBehaviour {
+
+    TrackIRUnity.TrackIRClient trackIRclient;
+    bool running = false;
+    string status, data;
+
+    // Use this for initialization
+    void Start () {
+        trackIRclient = new TrackIRUnity.TrackIRClient();
+        StartCamera();
+    }
+
+    void StartCamera()
+    {
+        if (trackIRclient != null && !running)
+        {                        // Start tracking
+            status = trackIRclient.TrackIR_Enhanced_Init();
+            running = true;
+        }
+    }
+
+    void StopCamera()
+    {
+        if (trackIRclient != null && running)
+        {                         // Stop tracking
+            status = trackIRclient.TrackIR_Shutdown();
+            running = false;
+        }
+    }
+
+    void OnEnable()
+    {
+        StartCamera();
+    }
+
+    void OnDisable()
+    {
+        StopCamera();
+    }
+
+    void OnApplicationQuit()
+    {                              
+        StopCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trackIRclient != null && running)
+        {
+
+
+        }
+    }
+}
